2ce05d904343065204c14ad505a03a1fe1978aa7
Yue Ning <yuefuturening@gmail.com>
2014-06-19 12:36:06 -0400

    Update TwoSumSol.cpp

--- 2Sum/TwoSumSol.cpp
+++ 2Sum/TwoSumSol.cpp
@@ -8,7 +8,7 @@
 #include <stdio.h>
 using namespace std;
 vector<int> twoSum(vector<int> &numbers, int target) {
-        int len=numbers.size();
+        int len = numbers.size();
         vector<int> index;
         vector<int> substract (len);
         for (int i = 0; i < len; i++){
@@ -26,8 +26,8 @@ vector<int> twoSum(vector<int> &numbers, int target) {
 }
 int main(){
 	vector<int> resultindex;
-	int myints[4]={2,7,8,9};
-	vector<int> inputarray(myints, myints + sizeof(myints)/sizeof(int));
+	int myints[4] = {2,7,8,9};
+	vector<int> inputarray(myints, myints + sizeof(myints) / sizeof(int));
 	resultindex = twoSum(inputarray,17);
 	for (int i = 0; i < resultindex.size(); i++)
 		printf("index:%d, ", resultindex[i]);
