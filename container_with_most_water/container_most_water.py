27a066504dda0c2ff252b1fce2475bdb6fc74a09
yning <yuefuturening@gmail.com>
2014-06-20 11:59:54 -0400

    new solution

---
+++ container_with_most_water/container_most_water.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import cPickle, string, numpy, getopt, sys, random, time, re, pprint,argparse
+__author__ = ['Yue']
+__version__ = "0.0.1"
+def main():
+	# number of elements in the array
+    n = 10
+    intlist = list()
+    # randomly generate integer elements for the array
+    for i in range(0,n):
+    	intlist.append(random.randint(1,100))
+
+    maxarea = 0
+    max_i = 0
+    max_j = 0
+    # iterate all possible solution to find the maximum area of rectangle min(ai, aj) * abs(j - i)
+    for i in range(0,n):
+    	for j in range(i+1,n):
+    		min_int = min(intlist[i],intlist[j])
+    		area = min_int * abs(j-i)
+    		if area > maxarea:
+    			max_i = i
+    			max_j = j
+    			maxarea = area
+    print "a[1] to a[n] is "+ str(intlist)
+    print "Maximum water in the container: %s, from a[%s] to a[%s]" %(maxarea, max_i, max_j)
+
+if __name__ == '__main__':
+    main()
