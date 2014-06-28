a6118caaae791b8f66ef35ef09dd2a4eb9a51ff3
yning <yuefuturening@gmail.com>
2014-06-28 12:21:21 -0400

    new solution

---
+++ string_to_integer/string_to_integer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import cPickle, string, numpy, getopt, sys, random, time, re, pprint
+__author__ = ['Yue']
+__version__ = "0.0.1"
+# Reference: http://www.programcreek.com/2012/12/leetcode-string-to-integer-atoi/
+def string_to_int(mystr):
+	print mystr
+	if len(mystr) == 0:
+		return 0
+	mystr = mystr.strip()
+
+	fg = '+'
+	i = 0
+	if mystr[0]=='-':
+		fg = '-'
+		i += 1
+	elif mystr[0] == '+':
+		i += 1
+
+	result = 0.0
+
+	while (i < len(mystr) and\
+		mystr[i] >= '0' and mystr[i] <= '9'):
+		result = result * 10.0 + float(int(mystr[i]) - int('0'))
+		i += 1
+
+	if fg == '-':
+		result = -result
+
+	if result > sys.maxint:
+		return sys.maxint
+
+	if result < (-sys.maxint-1):
+		return sys.minint
+
+	return int(result)
+def main():
+	mystr = sys.argv[1]
+	result = string_to_int(mystr)		
+	print result
+if __name__ == '__main__':
+    main()
+
