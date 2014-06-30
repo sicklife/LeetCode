63de338e2556afb459ae71c45aac8b6d9008b3a1
Yue Ning <yuefuturening@gmail.com>
2014-06-30 17:45:38 -0400

    Update integer_to_roman.py

--- integer_to_roman/integer_to_roman.py
+++ integer_to_roman/integer_to_roman.py
@@ -10,58 +10,60 @@ def main():
 	print myint
 	#I= 1;V = 5;X = 10;L = 50;C = 100;D = 500;M = 1000;
 	temp = myint
+	
+	result = ""
 	while (temp > 0):
 		if temp >= 1000:
 			first_divider = temp / 1000
 			if first_divider < 5:
 				while first_divider > 0:
-					sys.stdout.write("M")
+					result += "M"
 					first_divider -= 1
 			temp = temp % 1000
 		elif temp / 900 > 0:
-			sys.stdout.write("CM")	
+			result += "CM"	
 			temp = temp % 900	
 		elif temp >= 500:
-			sys.stdout.write("D")
+			result += "D"
 			temp = temp % 500
 		elif temp / 400 > 0:
-			sys.stdout.write("CD")
+			result += "CD")\
 			temp = temp % 400
 		elif temp >= 100:
 			first_divider = temp / 100
 			if first_divider < 5:
 				while first_divider > 0:
-					sys.stdout.write("C")
+					result += "C"
 					first_divider -= 1
 			temp = temp % 100
 		elif temp / 90 > 0:
-			sys.stdout.write("XC")
+			result += "XC"
 			temp = temp % 90
 		elif temp >= 50:
-			sys.stdout.write("L")
+			result += "L"
 			temp = temp % 50
 		elif temp / 40 > 0:
-			sys.stdout.write("XL")
+			result += "XL"
 			temp = temp % 40
 		elif temp >= 10:
 			first_divider = temp / 10
 			if first_divider < 5:
 				while first_divider > 0:
-					sys.stdout.write("X")
+					result += "X"
 					first_divider -= 1
 			temp = temp % 10
 		elif temp == 9:
-			sys.stdout.write("IX")
+			result += "IX"
 			temp = 0
 		elif temp >= 5:
-			sys.stdout.write("V")
+			result += "V"
 			temp = temp % 5
 		elif temp == 4:
-			sys.stdout.write("IV")
+			result += "IV"
 			temp = 0
 		else:
 			while temp > 0:
-				sys.stdout.write("I")
+				result += "I"
 				temp -= 1
 	print ""
 if __name__ == '__main__':
