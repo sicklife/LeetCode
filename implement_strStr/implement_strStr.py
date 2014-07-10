698895da03a4f870c3b1f78ec3fcf3ec7d394875
y.n. <yuefuturening@gmail.com>
2014-07-10 17:10:33 -0400

    Update implement_strStr.py

--- implement_strStr/implement_strStr.py
+++ implement_strStr/implement_strStr.py
@@ -9,26 +9,25 @@ def strStr(haystack, needle):
 	# Hanlde two special cases.
 	if needle == "":        return haystack
 	if haystack == "":      return None
-
+	
 	lenHaystack = len(haystack)
 	lenNeedle = len(needle)
 	begin = 0
-
+	
 	# Compare each substring as long as their length
 	# is >= the length of needle
 	while lenHaystack - begin >= lenNeedle:
-		for index in xrange(lenNeedle):
-			if haystack[begin+index] != needle[index]:
-				# Find a different char
-				break
-		else:
-			# Completely the same
-			return haystack[begin:]
-		
-		begin += 1
-
+	    for index in xrange(lenNeedle):
+	        if haystack[begin+index] != needle[index]:
+	            # Find a different char
+	            break
+	    else:
+	        # Completely the same
+	        return haystack[begin:]
+	    
+	    begin += 1
+	
 	return None
-
 def main():
 	hs = sys.argv[0]
 	needle = sys.argv[1]
