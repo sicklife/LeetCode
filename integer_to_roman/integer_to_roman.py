a461c81bf354c51ca3093db08490813e3ed61e90
李际朝 <sww@MacBook-Air.local>
2019-04-25 23:03:13 +0800

    update integer_to_roman

--- integer_to_roman/integer_to_roman.py
+++ integer_to_roman/integer_to_roman.py
@@ -1,72 +1,36 @@
 #!/usr/bin/python
-import cPickle, string, numpy, getopt, sys, random, time, re, pprint
-__author__ = ['Yue']
-__version__ = "0.0.1"
+__author__ = ['Zhao']
+__version__ = "0.0.2"
 
-def main():
-	mystr = sys.argv[1]
-	print mystr
-	myint = int(mystr)
-	print myint
-	#I= 1;V = 5;X = 10;L = 50;C = 100;D = 500;M = 1000;
-	temp = myint
-	
-	result = ""
-	while (temp > 0):
-		if temp >= 1000:
-			first_divider = temp / 1000
-			if first_divider < 5:
-				while first_divider > 0:
-					result += "M"
-					first_divider -= 1
-			temp = temp % 1000
-		elif temp / 900 > 0:
-			result += "CM"	
-			temp = temp % 900	
-		elif temp >= 500:
-			result += "D"
-			temp = temp % 500
-		elif temp / 400 > 0:
-			result += "CD")\
-			temp = temp % 400
-		elif temp >= 100:
-			first_divider = temp / 100
-			if first_divider < 5:
-				while first_divider > 0:
-					result += "C"
-					first_divider -= 1
-			temp = temp % 100
-		elif temp / 90 > 0:
-			result += "XC"
-			temp = temp % 90
-		elif temp >= 50:
-			result += "L"
-			temp = temp % 50
-		elif temp / 40 > 0:
-			result += "XL"
-			temp = temp % 40
-		elif temp >= 10:
-			first_divider = temp / 10
-			if first_divider < 5:
-				while first_divider > 0:
-					result += "X"
-					first_divider -= 1
-			temp = temp % 10
-		elif temp == 9:
-			result += "IX"
-			temp = 0
-		elif temp >= 5:
-			result += "V"
-			temp = temp % 5
-		elif temp == 4:
-			result += "IV"
-			temp = 0
-		else:
-			while temp > 0:
-				result += "I"
-				temp -= 1
-	print ""
-if __name__ == '__main__':
-    main()
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        symbols = "MDCLXVI"
+        steps = [1000, 500, 100, 50, 10, 5, 1]
 
 
+
+        result = ""
+
+
+        cur = 0
+        while cur < len(steps):
+            mod = num // steps[cur]
+            if mod < 4:
+                result += symbols[cur] * mod
+            elif mod == 4:
+                result = result + symbols[cur] + symbols[cur-1]
+            elif mod == 5:
+                result = result + symbols[cur-1]
+            elif 5<mod<9:
+                result = result + symbols[cur-1] + symbols[cur]*(mod-5)
+            else:
+                result = result + symbols[cur] + symbols[cur-2]
+            # print "====={}====".format(result)
+            num = num % steps[cur]
+            cur += 2
+
+        return result
