21910f1fcc42d2a3f7b26ac74e616e097b030b83
sicklife <tubkninght@gmail.com>
2019-04-26 21:43:29 +0800

    update longest_comman_prefix

--- longest_common_prefix/longest_common_prefix.py
+++ longest_common_prefix/longest_common_prefix.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python
 import cPickle, string, numpy, getopt, sys, random, time, re, pprint
-__author__ = ['Yue']
-__version__ = "0.0.1"
+__author__ = ['Zhao']
+__version__ = "0.0.2"
 
-def main():
-    mystrs = list()
-    mystrs.append("ababc")
-    mystrs.append("abcbca")
-    mystrs.append("acaba")
-    #mystrs.append("ab")
-    print mystrs
-
-    shortest_len = 5000
-    for i in range(0, len(mystrs)):
-        if len(mystrs[i]) < shortest_len:
-            shortest_len = len(mystrs[i])
-
-    #print "shortest length:%s" % shortest_len
-    longest = 0
-    i = 0
-    while(i < shortest_len):
-        counter = 0
-        for j in range(1, len(mystrs)):
-            if mystrs[j][i] != mystrs[j-1][i]:
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        common_prefix = ""
+        for t in zip(*strs):
+            if len(set(t)) > 1:
                 break
-            counter += 1
-        if counter < len(mystrs) - 1:
-            break
-        else:
-            i += 1
-            
-    longest = i
-    #print longest
+            else:
+                common_prefix += t[0]
 
-    print "Longest Common Prefix is %s" % mystrs[0][0:longest]
-if __name__ == '__main__':
-    main()
+        return common_prefix
+
