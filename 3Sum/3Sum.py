d7ba718fbe2d8ea13618020eda5cee103b489f89
sicklife <tubkninght@gmail.com>
2019-04-27 13:48:18 +0800

    update 3sum

--- 3Sum/3Sum.py
+++ 3Sum/3Sum.py
@@ -1,45 +1,37 @@
 #!/usr/bin/python
 import cPickle, string, numpy, getopt, sys, random, time, re, pprint
-__author__ = ['Yue']
-__version__ = "0.0.1"
+__author__ = ['Zhao']
+__version__ = "0.0.2"
 
-#Reference: http://leetcode.com/2010/04/finding-all-unique-triplets-that-sums.html
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            # 这里避免重复
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            # 设置左右游标
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    # 避免重复
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
 
-def triple_sum(mylist):
-	print mylist
-	mylist = sorted(mylist)
-	#print mylist
-	result = list()
-	n = len(mylist)
 
-	for i in range(0, n):
-		j = i + 1
-		k = n - 1
-		while (j < k):
-			two_sum = mylist[i]+mylist[j]
-			if two_sum + mylist[k] < 0:
-				j += 1
-			elif two_sum + mylist[k] > 0:
-				k -= 1
-			else:
-				temp = list()
-				temp.append(mylist[i])
-				temp.append(mylist[j])
-				temp.append(mylist[k])
-				result.append(temp)
-				j += 1
-				k -= 1
-
-	return result
-
-def main():
-	inputlist = list()
-	for i in range(0,20):
-		inputlist.append(random.randint(-20,20))
-	result = triple_sum(inputlist)	
-	print "Unique triples:"	
-	for triple in result:
-		print triple
-if __name__ == '__main__':
-    main()
 
