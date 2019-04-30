386ddca512fe887e7c3f0eeb9421c9e05965b32d
sicklife <tubkninght@gmail.com>
2019-04-30 22:25:32 +0800

    不理解

--- remove_nth_node_from_end_of_list/remove_nth_node_from_end.py
+++ remove_nth_node_from_end_of_list/remove_nth_node_from_end.py
@@ -35,6 +35,43 @@ def removeNthFromEnd(head, n):
 	    	first_ptr.next = first_ptr.next.next
 
 	    	return head
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        fast = slow = head
+        
+        for _ in range(n):
+            #print(fast.val)
+            fast = fast.next
+            #print(fast.val, fast.next.val, head.val, head.next.val)
+        #print(slow is head)
+        #print(fast == head)
+        if not fast:
+            return head.next
+        print(slow is head)
+        while fast.next:
+            #print(slow.val, fast.val)
+            fast = fast.next
+            slow = slow.next
+            # print(slow.val, fast.val)
+        print(slow is head)
+        print(head.val, head.next.val, head.next.next.val, head.next.next.next.val)
+        slow.next = slow.next.next
+        print(slow is head)
+        print(head.val, head.next.val, head.next.next.val, head.next.next.next.val)
+        return head
+        
 
 def main():
 	newnode = ListNode(2)
